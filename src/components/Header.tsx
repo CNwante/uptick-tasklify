6bbdf975afaace619ae09c94d5ad7d4cf0daabd5
Solomon Nwante <solomonchuks202@gmail.com>
2025-08-20 04:08:36 +0100

    fix: adjust header component layout
    - Add horizontal padding on the header element.
    - Remove vertical margin fromteh first child element.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,8 @@ import taskIcon from "../assets/task_icon.svg";
 
 export const Header = () => {
   return (
-    <header className="w-full px-2">
-      <div className="w-[80%] m-auto flex justify-between items-center my-4">
+    <header className="w-full px-2 py-4 bg-white">
+      <div className="w-[80%] m-auto flex justify-between items-center">
         <div className="flex items-center">
           <img src={taskIcon} alt="Task icon image" className="w-12 h-10  "/>
           <h1 className="font-bold text-xl">Tasklify</h1>
