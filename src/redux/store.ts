fe04677475e1fe94f71941a3036f3d32af4a9cd4
Solomon Nwante <solomonchuks202@gmail.com>
2025-08-25 09:51:39 +0100

    feat: persist data to browser local storage

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { taskReducer } from "../features/tasks/tasksSlice";
 import { taskFormReducer } from "../features/tasks/taskFormSlice";
+import { loadTasks, saveTasks } from "../utils/storage";
 
 export const store = configureStore({
   reducer: {
     tasks: taskReducer,
     taskForm: taskFormReducer,
   },
+  preloadedState: {
+    tasks: {
+      items: loadTasks(),
+    }
+  }
 });
+
+store.subscribe(() => {
+  const state = store.getState();
+  saveTasks(state.tasks.items);
+})
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
