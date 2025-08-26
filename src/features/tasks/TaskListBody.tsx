5f49e4a19fd3c918b7bd29b34c10a7590412753c
Solomon Nwante <solomonchuks202@gmail.com>
2025-08-26 06:42:34 +0100

    refactor(tasks): render TaskListBody with selectVisibleTasks

--- src/features/tasks/TaskListBody.tsx
+++ src/features/tasks/TaskListBody.tsx
@@ -1,13 +1,13 @@
 import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 import { TaskCard } from "./TaskCard";
 import { TaskForm } from "./TaskForm";
-import { selectTasksByStatus, updateTask, addTask } from "./tasksSlice";
+import { updateTask, addTask, selectVisibleTasks } from "./tasksSlice";
 import { closeTaskForm } from "./taskFormSlice";
 import type { TaskListBodyProps } from "./types";
 
 export const TaskListBody: React.FC<TaskListBodyProps> = ({ status }) => {
   const dispatch = useAppDispatch();
-  const tasks = useAppSelector((s) => selectTasksByStatus(s, status));
+  const tasks = useAppSelector((s) => selectVisibleTasks(s, status));
   const editingTask = useAppSelector((s) => s.taskForm.editingTask);
   const isOpen = useAppSelector((s) => s.taskForm.formVisibility[status]);
 
